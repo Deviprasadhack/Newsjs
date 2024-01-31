0df2ee82f04b6891a4cba19b6ef902c2f9d6297a
Deviprasadhack <85698605+Deviprasadhack@users.noreply.github.com>
2024-01-31 19:17:28 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,4 +47,4 @@ bot.onText(/\/news (\w+)/, async (msg, match) => {
 });
 
 // Set webhook to GitHub pages
-bot.setWebHook('https://deviprasadhack.github.io/bot.js');
+bot.setWebHook('https://deviprasadhack.github.io/Newjs/bot.js');
